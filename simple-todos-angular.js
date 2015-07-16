630e821e1ec1d52b772da760ebf87fb3ef3c247f
marvinick <marviluw@gmail.com>
2015-07-15 22:32:40 -0700

    first meteor

---
+++ simple-todos-angular.js
@@ -0,0 +1,17 @@
+// simple-todos-angular.js
+if (Meteor.isClient) {
+
+    // This code only runs on the client
+    angular.module("simple-todos",['angular-meteor']);
+
+    angular.module("simple-todos").controller("TodosListCtrl", ['$scope',
+      function($scope){
+
+      $scope.tasks = [
+        { text: "This is task 1" },
+        { text: "This is task 2" },
+        { text: "This is task 3" }
+      ];
+
+    }]);
+}
